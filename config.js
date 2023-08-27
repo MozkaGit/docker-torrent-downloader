391f65c539dcc10ec6a6bc36b587b60bd8544bf7
MozkaGit <mozkaops@gmail.com>
2023-08-27 22:27:25 +0000

    Create config.js

---
+++ config.js
@@ -0,0 +1,14 @@
+const config = {
+  transmission: {
+    host: 'transmissionHost',
+    port: 9091,
+  },
+  ygg: {
+    baseUrl: 'https://www3.yggtorrent.wtf',
+    flareSolverUrl: 'http://localhost:8191',
+    username: 'username',
+    password: 'password',
+  },
+};
+
+module.exports = config;
